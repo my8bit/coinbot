44c580c95229699ae758727210328d97fa1c0043
my8bit <mail@my8bit.name>
2014-09-18 23:31:41 +0300

    add grunt mocha

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,14 +22,21 @@ module.exports = function(grunt) {
                 src: 'build/production.js',
                 dest: 'build/production.min.js'
             }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/**/*.js']
+            }
         }
-
     });
-
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-mocha-test');
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'uglify', 'mochaTest']);
 
 };
